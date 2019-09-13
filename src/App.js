7dcade37c827573189276cafb69dcad1e70a0ebe
Jose72 <jjjtony_72@hotmail.com>
2019-09-13 12:58:51 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Foodie Web
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://taller2-foodie.herokuapp.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Fodie App Server
         </a>
       </header>
     </div>
