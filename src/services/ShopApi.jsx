6db8f120e0e59d859095d143664f5df6c6cb5868
Jose Morelli <JJJTONY_72@hotmail.com>
2019-11-14 14:51:05 -0300

    Cleaner ShopApi

--- src/services/ShopApi.jsx
+++ src/services/ShopApi.jsx
@@ -12,118 +12,80 @@ axios.defaults.headers.common['Content-Type'] =  'application/json';
 class ShopApi extends React.Component {
 
     static getShops(pIndex, pSize){
-        return axios.get(API_URL + SHOPS_ROUTE, {
-            params: {
-                p: pIndex,
-                pSize: pSize
-            },
-            headers: {
-                Authorization: "Bearer " + Auth.getToken()
-            }
-        })
-            .then(res => {
-                if (res.status === 200) {
-                    return res.data;
-                } else {
-                    return Promise.reject(res.status.toString() + ': ' + res.statusText)
-                }
-            })
-            .catch(error => {
-                console.log(error);
-                return Promise.reject(error)
-            })
+
+        let params = {
+            p: pIndex,
+            pSize: pSize,
+        };
+
+        let headers = {
+            Authorization: Auth.getToken()
+        };
+
+        return CommonApi.get(API_URL + SHOPS_ROUTE, params, headers);
     }
 
     static deleteShop(id){
-        return axios.delete(API_URL + SHOP_ROUTE + '/' + id, {
-            headers: {
-                Authorization: "Bearer " + Auth.getToken()
-            }
-        })
-            .then(res => {
-                if (res.status === 200) {
-                    console.log("Delete Successful");
-                } else {
-                    return Promise.reject(res.status.toString() + ': ' + res.statusText)
-                }
-            })
-            .catch(error => {
-                console.log(error);
-                return Promise.reject(error)
-            })
+
+        let params = {};
+
+        let headers = {
+            Authorization: Auth.getToken()
+        };
+
+        return CommonApi.delete(API_URL + SHOP_ROUTE + '/' + id, params, headers);
     };
 
-    static modifyShop(shop, opts){
-        return axios.put(API_URL + SHOP_ROUTE + '/' + shop.id,
-            {
-                name: shop.name,
-                address: shop.address,
-                description: shop.description,
-                pictureUrl: shop.pictureUrl
-            },
-            {
-                headers: {
-                    Authorization: "Bearer " + Auth.getToken()
-                }
-            })
-            .then(res => {
-                if (res.status === 200) {
-                    return Promise.resolve('Update Successful')
-                } else {
-                    return Promise.reject(res.status.toString() + ': ' + res.statusText)
-                }
-            })
-            .catch(error => {
-                console.log(error);
-                return Promise.reject(error)
-            })
+    static modifyShop(shop){
+
+        let params = {};
+
+        let data = {
+            name: shop.name,
+            description: shop.description,
+            phone: shop.phone,
+            reputation: shop.reputation,
+            picture: shop.picture
+        };
+
+        let headers = {
+            Authorization: Auth.getToken()
+        };
+
+        return CommonApi.put(API_URL + SHOP_ROUTE + '/' + shop.id, params, headers, data);
     }
 
     static addShop(shop){
-        return axios.post(API_URL + SHOP_ROUTE,
-            {
-                name: shop.name,
-                address: shop.address,
-                phone: shop.phone,
-                description: shop.description,
-                pictureUrl: shop.pictureUrl
-            },
-            {
-                headers: {
-                    Authorization: "Bearer " + Auth.getToken()
-                }
-            })
-            .then(res => {
-                if (res.ok) {
-                    console.log("Add Successful");
-                } else {
-                    return Promise.reject(res.status.toString() + ': ' + res.statusText)
-                }
-            })
-            .catch(error => {
-                console.log(error);
-                return Promise.reject(error)
-            })
+
+        let params = {};
+
+        let data = {
+            name: shop.name,
+            address: shop.address,
+            description: shop.description,
+            phone: shop.phone,
+            picture: shop.picture
+        };
+
+        let headers = {
+            Authorization: Auth.getToken()
+        };
+
+        return CommonApi.post(API_URL + SHOP_ROUTE, params, headers, data);
     }
 
     static getShop(id){
-        return axios.get(API_URL + SHOP_ROUTE + '/' + id, {
-            headers: {
-                Authorization: "Bearer " + Auth.getToken()
-            }
-        })
-            .then(res => {
-                if (res.status === 200) {
-                    return res.data
-                } else {
-                    return Promise.reject(res.status.toString() + ': ' + res.statusText)
-                }
-            })
-            .catch(error => {
-                console.log(error);
-                return Promise.reject(error)
-            })
+
+        let params = {};
+
+        let headers = {
+            Authorization: Auth.getToken()
+        };
+
+        return CommonApi.get(API_URL + SHOP_ROUTE + '/' + id, params, headers);
     };
+
+
 }
 
 export {ShopApi}
